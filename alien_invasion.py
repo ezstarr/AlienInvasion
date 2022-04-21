3f29da8b58c62b691bf855df0a06fc015c2572f6
Esther Lee <xiaoxinxin@gmail.com>
2022-04-20 19:26:22 -0700

    test no1

---
+++ alien_invasion.py
@@ -0,0 +1,96 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship, BudgieCutOut
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        self.budgie = BudgieCutOut(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # helper methods begin with an underscore.
+            self._check_events()
+            self.ship.update()
+            self.budgie.update()
+            self._update_screen()
+
+    def _check_events(self):
+        """Respond to key-presses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # Checks KEYDOWN function
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            # Checks KEYUP function
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+            print(str(event))
+
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == pygame.K_RIGHT:
+            self.budgie.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.budgie.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.budgie.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.budgie.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.budgie.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.budgie.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.budgie.moving_up = False
+        elif event.key == pygame.K_DOWN:                
+            self.budgie.moving_down = False
+
+
+
+
+
+
+
+                # # Cancel budgie movement
+                # if event.key == pygame.K_r:
+                #     self.budgie.moving_right = False
+                # if event.key == pygame.K_w:
+                #     self.budgie.moving_left = False
+
+
+    def _update_screen(self):
+        """Redraws the screen during each pass through the loop"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.update()
+        self.budgie.update()
+        self.ship.blitme()
+        self.budgie.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ ship.py
@@ -0,0 +1,68 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ships position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
+    def blitme(self):
+            """Draw the ship at its current location."""
+            self.screen.blit(self.image, self.rect)
+
+
+class BudgieCutOut:
+
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/budgiecutout.png')
+        image_scale_factor = 0.4
+        self.image = pygame.transform.scale(self.image,
+                                            (self.image.get_width() * image_scale_factor,
+                                             self.image.get_height() * image_scale_factor))
+        self.rect = self.image.get_rect()
+
+        # start the budgie at the left
+        self.rect.center = self.screen_rect.center
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+        if self.moving_up:
+            self.rect.y -= 1
+        if self.moving_down:
+            self.rect.y += 1
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
